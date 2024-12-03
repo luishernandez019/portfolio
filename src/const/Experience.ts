51399afeb1678183ac2dc1cffa577fb1898ac7e5
Luis Hernández <luis.hernandez@devbox.com.mx>
2024-12-03 07:48:09 -0600

    add DevBox job experience

--- src/const/Experience.ts
+++ src/const/Experience.ts
@@ -1,6 +1,15 @@
 export const EXPERIENCE = [
   {
     id: 1,
+    position: "Ingeniero de Software",
+    company: "DevBox",
+    date: "Septiembre 2024 - Noviembre 2024",
+    description: `
+      Encargado de maquetar los wireframes proporcionados por el equipo de diseño creando interfaces responsivas y optimizadas. En el lado del backend, desarrollé endpoints para diferentes módulos requeridos por la aplicación web asegurando una integración fluida y buen rendimiento.
+    `
+  },
+  {
+    id: 2,
     position: "Programador",
     company: "Induxsoft",
     date: "Febrero 2024 - Mayo 2024",
@@ -9,7 +18,7 @@ export const EXPERIENCE = [
     `
   },
   {
-    id: 2,
+    id: 3,
     position: "Desarrollador Frontend",
     company: "Flink",
     date: "Enero 2022 - Julio 2023",
@@ -18,7 +27,7 @@ export const EXPERIENCE = [
     `
   },
   {
-    id: 3,
+    id: 4,
     position: "Diseñador UI",
     company: "Agavesoft (Intern)",
     date: "Agosto 2021 - Diciembre 2021",
